53020d2788db049ad6774d445b61c6d3b62ea848
Camis <cvn.camila@gmail.com>
2021-01-13 22:27:55 -0300

    Criação dos componentes header e footer;

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+/*foi tirado todo o html desse arquivo e colocado em outro arquivo chamado app.component.html e esse arquivo foi referenciado nesse arquivo aqui na linha 6*/
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  
+}
